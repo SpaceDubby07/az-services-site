47b0c08d9addd354fc8cfdfc2dc24215f30738c1
SpaceDubby07 <zclark006@outlook.com>
2025-01-28 10:37:43 -0500

    main

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwind from '@astrojs/tailwind';
+
+import vercel from '@astrojs/vercel';
+
+// https://astro.build/config
+export default defineConfig({
+  server: { port: 3000 },
+  integrations: [tailwind()],
+  adapter: vercel(),
+});
